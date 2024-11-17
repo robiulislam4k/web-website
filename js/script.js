696e9df4efbfefb61d4e81ae3ea3359827b339cf
robiulawal4k <robiulislam4797830@gmail.com>
2024-11-18 01:24:40 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+function navigateToPage() {
+    window.location.href = "http://127.0.0.1:5500/index1.html";
+  }
---
+++ js/script2.js
@@ -0,0 +1,4 @@
+
+  document.getElementById("myButton").addEventListener("click", function () {
+    window.location.href = "https://example.com";
+  });
